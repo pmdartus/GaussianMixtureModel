7a8c9978f5e6aa46378415229b4edbc907e9031b
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2014-11-12 17:44:06 +0800

    Exp part

--- libs/em.py
+++ libs/em.py
@@ -2,6 +2,28 @@ import numpy as np
 import kmeans as KMeans
 
 
+def pdf(data, mean, cov):
+    """
+    Return the probability density funct of sample data
+
+    algo link: http://www.wikiwand.com/fr/Loi_normale_multidimensionnelle
+
+    # PARAMS
+    npArray data:   sample datas
+    npArray mean:   list of means
+    npArray cov:    list of covariance matrices
+
+    # RETURN
+    npArray proba:  probability for each data
+    """
+    dim = len(mean)
+
+    leftPart = 1 / np.sqrt(np.power(2*np.pi, dim) * np.linalg.det(cov))
+    mDiff = data - mean
+    expPart = - (1/2) * np.transpose(mDiff) * np.linalg.inv(cov) * (mDiff)
+    return leftPart * np.exp(expPart)
+
+
 def initGaussianModel(data, K, maxIter=5, verbose=False):
     """
     Get initals parameters for the EM algorithm
